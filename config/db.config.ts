030bb77638a50a06f12d6663dbdcabd79537e866
user530 <m.timarsuev@gmail.com>
2023-12-06 21:04:59 +0300

    Config and typeorm modules initial setup

---
+++ config/db.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const dbConfig = registerAs('database', (): TypeOrmModuleOptions => {
+    return {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        synchronize: process.env.SYNC === 'true',
+        entities: []
+    };
+});
