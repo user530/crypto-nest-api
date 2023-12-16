ee359eab6cc5414202c2f55b666280b20ea48a6f
user530 <m.timarsuev@gmail.com>
2023-12-16 19:56:48 +0300

    Fixed CORS - only specified origin allowed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN
+  })
   await app.listen(3456);
 }
 bootstrap();
